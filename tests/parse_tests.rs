5d58990bfbe189adcf127928b694427eb3b1577c
Michael Budde <mbudde@gmail.com>
2017-03-26 15:24:31 +0200

    Add some pass/fail parsing tests

---
+++ tests/parse_tests.rs
@@ -0,0 +1,38 @@
+extern crate gettext_po;
+
+use std::fs;
+use std::io::Read;
+use std::path::PathBuf;
+
+fn po_files_in_dir(path: &str) -> Box<Iterator<Item=(PathBuf, Vec<u8>)>> {
+    Box::new(fs::read_dir(path)
+        .unwrap()
+        .filter_map(|entry| {
+            entry.ok().into_iter()
+                .filter(|e| e.file_type().map(|ft| ft.is_file()).unwrap_or(false))
+                .filter(|e| e.path().extension().map(|e| e == "po").unwrap_or(false))
+                .next()
+        })
+        .map(|entry| {
+            let path = entry.path();
+            let mut content = Vec::new();
+            fs::File::open(&path).unwrap().read_to_end(&mut content).unwrap();
+            (path, content)
+        }))
+}
+
+#[test]
+fn parse_pass() {
+    for (path, content) in po_files_in_dir("tests/parse-pass") {
+        println!("Parsing expecting success: {:?}", path);
+        assert!(gettext_po::parse(&content[..]).is_ok());
+    }
+}
+
+#[test]
+fn parse_fail() {
+    for (path, content) in po_files_in_dir("tests/parse-fail") {
+        println!("Parsing expecting error: {:?}", path);
+        assert!(gettext_po::parse(&content[..]).is_err());
+    }
+}
